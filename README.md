288d3a9f7abb4dba9d630026716767db56a472b8
EC2 Default User <ec2-user@ip-172-31-30-89.ec2.internal>
2025-08-14 18:38:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## YAML File Screenshot
+![YAML Screenshot](yaml_screenshot.png)
+# Netdata Monitoring with Docker
+
+## Objective
+Install Netdata and visualize live system and application performance metrics.
+
+## Tools
+- Netdata (Free, Open-Source monitoring tool)
+- Docker
+
+## Steps to Reproduce
+
+1. **Run Netdata container**
+   ```bash
+   ./run_netdata.sh
---
+++ run_netdata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Netdata Monitoring Setup Script using Docker
+
+# Run Netdata in Docker
+docker run -d --name=netdata \
+  -p 19999:19999 \
+  --cap-add SYS_PTRACE \
+  --security-opt apparmor=unconfined \
+  -v netdata-config:/etc/netdata \
+  -v netdata-lib:/var/lib/netdata \
+  -v netdata-cache:/var/cache/netdata \
+  -v /etc/passwd:/host/etc/passwd:ro \
+  -v /etc/group:/host/etc/group:ro \
+  -v /proc:/host/proc:ro \
+  -v /sys:/host/sys:ro \
+  -v /etc/os-release:/host/etc/os-release:ro \
+  netdata/netdata
